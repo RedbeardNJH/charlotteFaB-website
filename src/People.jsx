f840cbbd58ad02ce99a3c8b7440229b0ad41841a
Jacob K <jwkilcrease2004@gmail.com>
2025-12-01 16:27:00 -0500

    People content

--- src/People.jsx
+++ src/People.jsx
@@ -1,8 +1,37 @@
 import './styles/App.css'
 
 function People() {
-    return(
+    return (
         <>
+            <h2>The People of FAB CLT</h2>
+            <h3>The Judges</h3>
+            <p>Our judges help to ensure a fair play experience for everyone as well as ensuring sportsmen like conduct. The judges are not only responsible for ensuring the rules of the game are upheld but also run all of the events from weekly armories to larger Pro Quest and Road to Nationals.</p>
+            <figure class="people">
+                <h4>Jim VanFleet</h4>
+                <img src="" alt="(Picture of Jim)" />
+                <ul>
+                    <li>Level 2 Judge</li>
+                    <li>Discord: bigfleet</li>
+                </ul>
+            </figure>
+            <figure class="people">
+                <h4>Ashlyn Slattery</h4>
+                <img src="" alt="(Picture of Ashlyn)" />
+                <ul>
+                    <li>Level 1 Judge</li>
+                    <li>Discord: slatty_v3</li>
+                </ul>
+            </figure>
+            <h3>The Locals</h3>
+            <p>The ones you will laugh with and the ones you will one day face against. Our locals always look to bring a fun time and their A-game to any event you may see them in</p>
+            <figure class="people">
+                <h4>Noah Harden</h4>
+                <img src="" alt="(Picture of Noah)" />
+            </figure>
+            <figure class="people">
+                <h4>Jacob Kilcrease</h4>
+                <img src="" alt="(Picture of Jacob)" />
+            </figure>
         </>
     );
 }
