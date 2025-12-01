f9ae5a27ff716aedae8e3e475e7763b3b66430a5
RedbeardNJH <154090043+RedbeardNJH@users.noreply.github.com>
2025-12-01 00:36:46 -0500

    Filling basic pages with content

--- src/Locations.jsx
+++ src/Locations.jsx
@@ -12,7 +12,7 @@ function Locations() {
                 <p>Last updated: 11/30/2025</p>
             </div>
             <div class="frame">
-                <div>
+                <div class="left_side">
                     <iframe
                         width="600"
                         height="450"
@@ -24,21 +24,32 @@ function Locations() {
                         title="Space Needle map"
                     />
                 </div>
-                <div>
+                <div class="right_side">
                     <h3>Get Some Game</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A rerum necessitatibus laboriosam eveniet, ex veritatis dolores error fugiat deserunt amet labore magnam iusto iste corporis unde distinctio similique? Repellat, fugiat.</p>
+                    <p><strong>Wednesday</strong>: 6:30 - 10:00pm <br /><strong>Format</strong>: Classic Constructed, Silver Age</p>
                 </div>
             </div>
-            <p>Skroikin it bigly</p>
-            <iframe
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                loading="lazy"
-                allowFullScreen
-                referrerPolicy="no-referrer-when-downgrade"
-                src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Cosmic+Hearth,Charlotte+NC`}
-                title="Space Needle map"
-            />
+            <br />
+            <div class="frame">
+                <div class="left_side">
+                    <h3>Cosmic Hearth</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptate a neque inventore sed quidem autem eius molestiae fugit, tempora natus similique quia at vel excepturi saepe id. Laborum, sed.</p>
+                    <p><strong>Friday</strong>: 6:30 - 10:00pm <br /><strong>Format</strong>: Classic Constructed</p>
+                </div>
+                <div class="right_side">
+                    <iframe
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        loading="lazy"
+                        allowFullScreen
+                        referrerPolicy="no-referrer-when-downgrade"
+                        src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=Cosmic+Hearth,Charlotte+NC`}
+                        title="Space Needle map"
+                    />
+                </div>
+            </div>
         </>
     );
 }
