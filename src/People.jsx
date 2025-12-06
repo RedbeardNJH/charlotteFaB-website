42aec5c5c32bd3eb769be542d8a0478fdbf88c3c
Jacob K <jwkilcrease2004@gmail.com>
2025-12-06 15:18:18 -0500

    Adding people

--- src/People.jsx
+++ src/People.jsx
@@ -4,8 +4,11 @@ function People() {
     return (
         <>
             <h2>The People of FAB CLT</h2>
+
             <h3>The Judges</h3>
+
             <p>Our judges help to ensure a fair play experience for everyone as well as ensuring sportsmen like conduct. The judges are not only responsible for ensuring the rules of the game are upheld but also run all of the events from weekly armories to larger Pro Quest and Road to Nationals.</p>
+            
             <figure class="people">
                 <h4>Jim VanFleet</h4>
                 <img src="" alt="(Picture of Jim)" />
@@ -14,16 +17,20 @@ function People() {
                     <li>Discord: bigfleet</li>
                 </ul>
             </figure>
+
             <figure class="people">
                 <h4>Ashlyn Slattery</h4>
-                <img src="" alt="(Picture of Ashlyn)" />
+                <img src="images/ash_scerve_win.png" alt="(Picture of Ashlyn)" class="people-pic"/>
                 <ul>
                     <li>Level 1 Judge</li>
                     <li>Discord: slatty_v3</li>
                 </ul>
             </figure>
+
             <h3>The Locals</h3>
+
             <p>The ones you will laugh with and the ones you will one day face against. Our locals always look to bring a fun time and their A-game to any event you may see them in</p>
+            
             <figure class="people">
                 <h4>Noah Harden</h4>
                 <img src="" alt="(Picture of Noah)" />
