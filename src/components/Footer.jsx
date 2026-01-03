6c99186a31d80288a158ac1bb90d7260e55563fb
RedbeardNJH <154090043+RedbeardNJH@users.noreply.github.com>
2026-01-02 20:22:47 -0500

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
         <footer>
-            <p class="footerP">Designed by Noah Harden and Jacob Kilcrease</p>
-            <p class="footerP"><a href="https://github.com/RedbeardNJH/charlotteFaB-website">GitHub</a> | Issues with the site? Contact: doggo4d on discord</p>
+            <p>Designed by Noah J Harden and Jacob Kilcrease</p>
+            <p><a href="https://github.com/RedbeardNJH/charlotteFaB-website">GitHub</a> | Issues with the site? Contact: doggo4d on discord</p>
         </footer>
     );
 }
